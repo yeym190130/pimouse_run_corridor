88032a2cd958918e7f3fbc7d430a4018f6398ada
yeym190130 <masaya.yamamoto@youtheng.co.jp>
2019-02-15 02:16:09 +0000

    test wallstop try1

---
+++ test/travis_test_wall_stop.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import unittest, rostest
+import rosnode, rospy
+import time
+
+class WallStopTest(unittest.TestCase):
+	def set_and_get(self,lf,ls,rs,rf):
+		with open("/dev/rtlightsensors0","w") as f:
+			f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
+
+		time.sleep(3)
+
+		with open("/dev/rtmotor_raw_l0","r") as lf,\
+		     open("/dev/rtmotor_raw_r0","r") as rf:
+			left=int(lf.readline().rstrip())
+			right=int(rf.readline().rtstrip())
+
+		return left, right
+
+	def test_io(self):
+		left, right=self.set_set_and_get(400,100,100,0) #total:600
+		self.assertTrue(left==0 and right==0,"can't stop")
+
+		left, right=set_and_get(400,0,0,99) #total:499
+		self.assertTrue(left!=0 and right!=0,"can't move again")
+
+		left, right=set_and_get(150,0,200,150) #total:500
+		self.assertTrue(left==0 and right==0,"can't stop")
+
+if __name__='__main__':
+	time.sleep(3)
+	rospy.init_node('travis_test_wall_stop')
+	rospy.rosrun('pimouse_run_corridor','travis_test_wall_stop',WallStopTest)
+
+
