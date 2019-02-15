ab60713f3fee02e9d3e6a27dce304c7aa9154b6f
yeym190130 <masaya.yamamoto@youtheng.co.jp>
2019-02-15 01:37:57 +0000

    copied test and launch

---
+++ test/travis_package_make.bash
@@ -0,0 +1,10 @@
+#!/bin/bash -xve
+
+#sync and make
+rsync -av ./ ~/catkin_ws/src/pimouse_run_corridor/
+
+cd ~/catkin_ws/src/
+git clone --depth=1 https://github.com/yeym190130/pimouse_ros.git
+
+cd ~/catkin_ws
+catkin_make
