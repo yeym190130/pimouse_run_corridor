a3cc6976f9ab863d11b7aeadeccf4340c3e9a607
yeym190130 <masaya.yamamoto@youtheng.co.jp>
2019-02-15 02:29:08 +0000

    test wallstop try2

--- test/travis_test_wall_stop.py
+++ test/travis_test_wall_stop.py
@@ -5,7 +5,7 @@ import time
 
 class WallStopTest(unittest.TestCase):
 	def set_and_get(self,lf,ls,rs,rf):
-		with open("/dev/rtlightsensors0","w") as f:
+		with open("/dev/rtlightsensor0","w") as f:
 			f.write("%d %d %d %d\n" % (rf,rs,ls,lf))
 
 		time.sleep(3)
@@ -13,18 +13,18 @@ class WallStopTest(unittest.TestCase):
 		with open("/dev/rtmotor_raw_l0","r") as lf,\
 		     open("/dev/rtmotor_raw_r0","r") as rf:
 			left=int(lf.readline().rstrip())
-			right=int(rf.readline().rtstrip())
+			right=int(rf.readline().rstrip())
 
 		return left, right
 
 	def test_io(self):
-		left, right=self.set_set_and_get(400,100,100,0) #total:600
+		left, right=self.set_and_get(400,100,100,0) #total:600
 		self.assertTrue(left==0 and right==0,"can't stop")
 
-		left, right=set_and_get(400,0,0,99) #total:499
+		left, right=self.set_and_get(400,0,0,99) #total:499
 		self.assertTrue(left!=0 and right!=0,"can't move again")
 
-		left, right=set_and_get(150,0,200,150) #total:500
+		left, right=self.set_and_get(150,0,200,150) #total:500
 		self.assertTrue(left==0 and right==0,"can't stop")
 
 if __name__='__main__':
